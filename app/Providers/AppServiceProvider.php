81f43d9065aaa282f99ef5744794b3824b9c049f
iksor <iksorek@gmail.com>
2021-05-12 23:46:10 +0100

    ADD admin and super admin role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole('super_admin')) {
+                return true;
+            }
+        });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        Permission::create(['name' => 'verify user']);
+        Permission::create(['name' => 'delete user']);
+        Permission::create(['name' => 'update user']);
+        Permission::create(['name' => 'add user']);
+        $admin = Role::create(['name' => 'admin']);
+        $admin->givePermissionTo('verify user');
+        $admin->givePermissionTo('delete user');
+        $admin->givePermissionTo('update user');
+        $admin->givePermissionTo('add user');
+
+        $super_admin = Role::create(['name' => 'super_admin']);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'iksor',
+            'email' => 'dev@mrogon.co.uk',
+        ]);
+        $user->assignRole('super_admin');
+
+
+
+
     }
 }
+
