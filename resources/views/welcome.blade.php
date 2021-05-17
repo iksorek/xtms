53f6e26eadac8448d45f1d1372011f97a6f576a8
iksor <iksorek@gmail.com>
2021-05-17 23:14:25 +0100

    FIX CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>TMS</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset("css/app.css")}}">
-    <!-- Styles -->
-
-
 </head>
 <body class="antialiased">
 <div
