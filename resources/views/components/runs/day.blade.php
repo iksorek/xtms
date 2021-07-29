cbb55f6a7f14b50b1e4e5e1710c5657b2c7bc9ad
iksor <iksorek@gmail.com>
2021-07-29 22:21:33 +0100

    Fix Icons hiding in mobile view

--- resources/views/components/runs/day.blade.php
+++ resources/views/components/runs/day.blade.php
@@ -1,26 +1,28 @@
 <div class="flex bg-blue-300 my-1 text-xsm md:text-sm">
 
-    <div class="h-20 m-1 bg-blue-200 p-2 flex-shrink-0">
+    <div class="h-full border border-r m-1 bg-blue-200 p-2 flex-shrink-0">
         <p> {{date("l", strtotime($day))}}</p>
         <p>{{$day}} </p>
     </div>
     <div class="w-full p-2">
         @forelse($vehicles as $vehicle)
-            <div class="border border-2 w-full m-1 p-2">
-                <div class="w-full font-bold text-center">{{$vehicle->reg}} {{$vehicle->make}}
-                    - {{$vehicle->model}}</div>
+            <div class="w-full m-1 p-2">
+                <p class="w-1/2 font-bold text-center mx-auto bg-blue-400">{{$vehicle->reg}} {{$vehicle->make}}
+                    - {{$vehicle->model}}</p>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full">
 
                     @foreach($vehicle->Runs as $oneRun)
                         <div class="mx-1 p-2
                          border border-2 relative">
                             <div class="absolute top-1 right-0.5 flex">
-                            <x-runs.edit-icon :id="$oneRun->id"/>
-                            <x-runs.view-icon :id="$oneRun->id"/>
+                                <x-runs.edit-icon :id="$oneRun->id"/>
+                                <x-runs.view-icon :id="$oneRun->id"/>
                             </div>
-                            <p>{{$oneRun->postcode_from}} to {{$oneRun->postcode_to}}({{$oneRun->start_time}})</p>
-                            <p>{{substr($oneRun->start_time, -8)}} - {{substr($oneRun->finish_est, -8)}}
-                                {{$oneRun->back_est ? "(".substr($oneRun->back_est, -8).")" : 'No Estimates'}}</p>
+                            <div class="pr-10">
+                                <p>{{$oneRun->postcode_from}} to {{$oneRun->postcode_to}}({{$oneRun->start_time}})</p>
+                                <p>{{substr($oneRun->start_time, -8)}} - {{substr($oneRun->finish_est, -8)}}
+                                    {{$oneRun->back_est ? "(".substr($oneRun->back_est, -8).")" : 'No Estimates'}}</p>
+                            </div>
                         </div>
                     @endforeach
 
@@ -28,38 +30,40 @@
             </div>
 
         @empty
-            <div class="inline-block h-20 m-1 bg-blue-400 p-2">
+            <div class="inline-block h-20 p-2">
                 <p>No runs fully planned yet</p>
             </div>
         @endforelse
-        <div class="border border-2 w-full m-1 p-2">
-            <div class="w-full font-bold text-center">Not assigned or requested</div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full">
-                @forelse($runsWithoutVehicle as $run)
-                    <div class="mx-1 border border-2 bg-yellow-400 p-2 relative">
+        <div class="w-full m-1 p-2">
+            @if($runsWithoutVehicle->count() != 0)
+                <div class="w-full font-bold text-center">Not assigned or requested</div>
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full">
+                    @forelse($runsWithoutVehicle as $run)
+                        <div class="mx-1 border border-2 bg-yellow-400 p-2 relative">
 
-                        <div class="relative">
-                            <div class="absolute top-1 right-0.5 flex">
-                                <x-runs.edit-icon :id="$run->id"/>
-                                <x-runs.view-icon :id="$run->id"/>
+                            <div class="relative">
+                                <div class="absolute top-1 right-0.5 flex">
+                                    <x-runs.edit-icon :id="$run->id"/>
+                                    <x-runs.view-icon :id="$run->id"/>
+                                </div>
+                                <div class="pr-10">
+                                    <p>{{$run->postcode_from}} to {{$run->postcode_to}}</p>
+                                    <p>
+                                        {{substr($run->start_time, -8)}} - {{substr($run->finish_est, -8)}}
+
+                                    </p>
+                                    <p class="run_details_missing_badge">{{$run->customer_id ? '' : 'No customer'}} {{$run->vehicle_id ?: 'No vehicle'}}</p>
+                                </div>
+
                             </div>
-                            <p>{{$run->postcode_from}} to {{$run->postcode_to}}</p>
-                            <p>
-                                {{substr($run->start_time, -8)}} - {{substr($run->finish_est, -8)}}
 
-                            </p>
-                            <p class="run_details_missing_badge">{{$run->customer_id ? '' : 'No customer'}} {{$run->vehicle_id ?: 'No vehicle'}}</p>
 
                         </div>
-
-
-
-
-                    </div>
-                @empty
-                    All run assigned to vehicle.
-                @endforelse
-            </div>
+                    @empty
+                        All run assigned to vehicle.
+                    @endforelse
+                </div>
+            @endif
         </div>
     </div>
 
