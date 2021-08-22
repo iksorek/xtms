5b8cf4a34bb2bf5002e6a64883043b8690a98033
iksor <iksorek@gmail.com>
2021-08-22 21:24:13 +0100

    API running

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/quote', function (Request $request) {
-//    return \App\Models\User::where('api_key', $request->api_key)->first();
-    return 'DUPA';
+    $result = getQuote($request->postcode_from, $request->postcode_to);
+    return $request;
+
 });
