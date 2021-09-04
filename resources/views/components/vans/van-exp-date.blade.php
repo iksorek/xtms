1044ad9e9edfc62e6607eb845029dd7c724ebf15
iksor <iksorek@gmail.com>
2021-09-05 00:10:28 +0100

    REMOVE rounded class

--- resources/views/components/vans/van-exp-date.blade.php
+++ resources/views/components/vans/van-exp-date.blade.php
@@ -4,7 +4,7 @@
  {{ ($mode == 'info') ? 'bg-green-500' : ''}}
  {{ ($mode == 'warning') ? 'bg-yellow-500' : ''}}
  {{ ($mode == 'err') ? 'bg-red-500' : ''}}
- mx-1 px-3 rounded-2xl text-center has-tooltip">
+ mx-0 px-3 border border-black  text-center has-tooltip">
     @if($type)
         {{$type}}
         <span class='tooltip text-{{$color}}-500 rounded shadow-lg p-1 bg-gray-100 text-red-500 -mt-6'>{{$tooltip}}</span>
