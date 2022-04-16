52e533bdd969b6731db10676001e4630d37f1728
iksor <iksorek@gmail.com>
2022-04-16 23:05:03 +0100

    EDIT Run status change style

--- resources/views/livewire/run-status-change-buttons.blade.php
+++ resources/views/livewire/run-status-change-buttons.blade.php
@@ -1,14 +1,14 @@
-<div class="flex">
+<div class="flex flex-col">
 
 
     <button
-        class="w-1/3 bg-yellow-300 m-1 {{$status == 'new' ? 'font-extrabold bg-yellow-500 outline-solid' : 'bg-yellow-300'}}"
+        class="button-status {{$status == 'new' ? 'font-extrabold bg-yellow-500 outline-solid' : 'bg-yellow-300'}}"
         wire:click="setNewStatus('new')">New
     </button>
-    <button class="w-1/3 bg-red-300 m-1 {{$status == 'requested' ? 'font-extrabold bg-red-500 outline-solid' : 'bg-red-300'}}"
+    <button class="button-status {{$status == 'requested' ? 'font-extrabold bg-red-500 outline-solid' : 'bg-red-300'}}"
             wire:click="setNewStatus('requested')">Requested
     </button>
-    <button class="w-1/3 m-1 {{$status == 'confirmed' ? 'font-extrabold bg-green-500 outline-solid' : 'bg-green-300'}}"
+    <button class="button-status text-sm {{$status == 'confirmed' ? 'font-extrabold bg-green-500 outline-solid' : 'bg-green-300'}}"
             wire:click="setNewStatus('confirmed')">Confirmed
     </button>
 </div>
