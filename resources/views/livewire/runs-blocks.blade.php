e396c1584963e26b759140e86e6729527dd5ec2b
iksor <iksorek@gmail.com>
2022-05-02 23:21:45 +0100

    ADD Display error message when no value entered

--- resources/views/livewire/runs-blocks.blade.php
+++ resources/views/livewire/runs-blocks.blade.php
@@ -2,10 +2,15 @@
 
     <x-jet-input type="number" wire:model="daysToCount" max="100" maxlength="3"/>
 
-    <x-jet-input type="button" class="button-like-link" wire:click="setDaysToCount({{$daysToCount}})" :disabled="$daysToCount == ''" value="Update"/>
+    <x-jet-input type="button" class="button-like-link" wire:click="setDaysToCount({{$daysToCount}})"
+                 :disabled="$daysToCount == ''" value="Update"/>
     @error('daysToCount') <p class="error">{{ $message }}</p>
     @else
-        <p>Showing planner for {{$daysToCount}} days:</p>
+        @if(!is_int($daysToCount))
+            <p class="error">Please enter a number</p>
+        @else
+            <p>Showing planner for {{$daysToCount}} days:</p>
+        @endif
         @enderror
 
         @foreach($dates as $day)
